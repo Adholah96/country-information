9a8f0dd5aecb2182837c30b9ddd34cc011d70afb
Adholah96 <adholah96@gmail.com>
2023-04-19 19:10:50 +0300

    update snapshots

--- src/__tests__/details.test.js
+++ src/__tests__/details.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import Details from '../__mocks__/Details';
 
-describe('Companies', () => {
+describe('Nations', () => {
   it('renders correctly', () => {
     const tree = renderer.create(<Details />).toJSON();
     expect(tree).toMatchSnapshot();
